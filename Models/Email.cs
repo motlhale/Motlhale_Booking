f996feb8ca2f56658068fb431e58013e8ce20cf4
motlhale <41773917+motlhale@users.noreply.github.com>
2018-07-27 09:40:14 +0200

    from documents
    entity framework models

---
+++ Models/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingProject.Models
+{
+    public class Email
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
